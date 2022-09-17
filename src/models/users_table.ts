5ecc8ce91d00c3b89ea88cd4b770736c71125aad
natsir-productions <natsir.productions@gmail.com>
2022-09-17 20:47:39 +0700

    field name email password cant be null

--- src/models/users_table.ts
+++ src/models/users_table.ts
@@ -19,13 +19,17 @@ export default function setupUsersTable() {
         unique: true
       },
       name: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        allowNull: false
       },
       email: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
       },
       password: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        allowNull: false
       }
     },
     {
